b97d22da309ef8cdb6324896769f6f24433b52a6
Kevin Wenger <wenger.kev@gmail.com>
2021-09-08 14:38:42 +0200

    add static code analyzers

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude('vendor')
+;
+
+$config =  new PhpCsFixer\Config;
+return $config
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_superfluous_phpdoc_tags' => false,
+//        'phpdoc_align' => ['align' => 'vertical', 'tags' => ['param', 'type', 'var']],
+    ])
+    ->setFinder($finder)
+;
